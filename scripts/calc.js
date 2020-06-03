a3f850704f74e4873a9b5e6bf2fc2ea24b027007
joffy69 <jcwgibbs@gmail.com>
2020-06-03 20:04:58 +0000

    commit ahead of shutdown

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,7 @@
 function addition(num1,num2) {
+    if(typeof(num1)=="number" && (typeof(num2)=="number")){
     return num1+num2;
+    } else {
+        return "Error!";
+    }
 }
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -7,5 +7,8 @@ describe("Calculator", function() {
             expect(addition(17,9)).toBe(26);
 
         });
+        it("should return an error if we don't apply two numbers", function(){
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        });
     });
 });
