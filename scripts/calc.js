b434d21c3ff4c6875be8f8ba29e7bf732d19567f
joffy69 <jcwgibbs@gmail.com>
2020-06-04 16:39:07 +0000

    clearing the cache causes the 403. restart and the code works, though the odd behavior that the cache clear was trying to stop remains

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,19 @@
-function addition(num1,num2) {
+Calculator=function(){
+    this.value=0;
+}
+
+Calculator.prototype.add=function(number){
+    if (typeof(number)== "number"){
+        this.value +=number;
+    } else {
+        alert("Error!");
+    }
+}
+
+/*function addition(num1,num2) {
     if(typeof(num1)=="number" && (typeof(num2)=="number")){
     return num1+num2;
     } else {
-        return "Error!";
+        alert("Error!");
     }
-}
+}*/
