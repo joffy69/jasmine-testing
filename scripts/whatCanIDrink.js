8df545c5bc0aaffd6606c1e82291ecbc37a3fbcb
joffy69 <jcwgibbs@gmail.com>
2020-06-05 15:13:56 +0000

    whatcanidrink written waiting for jasmine book

--- scripts/whatCanIDrink.js
+++ scripts/whatCanIDrink.js
@@ -1,9 +1,24 @@
 whatCanIDrink=function(age){
-    if  (age<0){
+   
+switch (age) {
+    case (age<0):
         return "Sorry. I can't tell what drink because that age is incorrect!";
-    } else if {
-
-    }
+        break;
+    case (age<14):
+        return "Drink Toddy";
+        break;
+    case (age<18):
+        return "Drink Coke";
+        break;
+    case (age<21):
+        return "Drink Beer";
+        break;
+    case (age<130):
+        return "Drink Whiskey";
+        break;
+    default:
+        return "Sorry, I can't tell what drink because that age is incorrect!";
+        break;
 }
 
-/*use cases*/
+}
