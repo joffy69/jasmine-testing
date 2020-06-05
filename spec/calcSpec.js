b1ef8fc7dabf6859ed53f16af8f3b7c29e9c8189
joffy69 <jcwgibbs@gmail.com>
2020-06-05 14:36:45 +0000

    conclusion of test-driven development

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,17 +1,29 @@
 describe("Calculator", function() {
+beforeEach(function(){
+
+
+    calc=new Calculator;
+    });
     describe("Addition tests", function(){
         it("should return 42", function(){
-            expect(addition(20,22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect (calc.value).toBe(42);
+            /*expect(addition(20,22)).toBe(42);*/
         });
         it("should return 26", function(){
-            expect(addition(17,9)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect (calc.value).toBe(26);
+            /*expect(addition(17,9)).toBe(26);*/
 
         });
         it("should return an error if we don't apply two numbers", function(){
 
             spyOn(window, "alert");
-addition("Hitchhikers","Guide");
-expect(window.alert).toHaveBeenCalledWith("Error!");
+            calc.add("Hitchhikers");
+            /*addition("Hitchhikers","Guide");*/
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
 });
