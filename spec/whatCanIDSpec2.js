cfbf9e56ea0eeb02d751a96f23a4ea09cf9d85e1
joffy69 <jcwgibbs@gmail.com>
2020-06-11 11:35:38 +0000

    second go
    spec now fails 5 times

--- spec/whatCanIDSpec2.js
+++ spec/whatCanIDSpec2.js
@@ -1,7 +1,31 @@
 describe ("Drink", function(){
+    describe ("Unborn", function(){
+        it("should return sorry", function(){
+            expect(something(age)).toBe("Sorry. I can't tell what drink because that age is incorrect!");
+        });
+    });
     describe ("Toddy", function(){
         it("should return toddy", function(){
             expect(something(age)).toBe("Drink Toddy");
         });
     });
+
+    describe ("Coke", function(){
+        it("should return coke", function(){
+            expect(something(age)).toBe("Drink Coke");
+        });
+    });
+
+    describe ("Beer", function(){
+        it("should return beer", function(){
+            expect(something(age)).toBe("Drink Beer");
+        });
+    });
+
+    describe ("Whiskey", function(){
+        it("should return whiskey", function(){
+            expect(something(age)).toBe("Drink Whiskey");
+        });
+    });
+
 });
