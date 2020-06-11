8bb9ecb3abf6eff9ac28597e6a6bca4f66efd9d6
joffy69 <jcwgibbs@gmail.com>
2020-06-11 11:30:17 +0000

    second go
    now something is not defined

--- spec/whatCanIDSpec2.js
+++ spec/whatCanIDSpec2.js
@@ -1,3 +1,7 @@
 describe ("Drink", function(){
-
+    describe ("Toddy", function(){
+        it("should return toddy", function(){
+            expect(something(age)).toBe("Drink Toddy");
+        });
+    });
 });
